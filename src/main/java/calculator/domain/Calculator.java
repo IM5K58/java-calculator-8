a1766d1e634974c0ef99c272dd441dfc9bb7306a
gitue <gitue11@gmail.com>
2025-10-20 11:03:56 +0900

    refactor: InputValidator에서 메소드 추가 분리, 코드 정리

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -14,19 +14,17 @@ public class Calculator {
     public void Calculate(String input){ //계산 과정 총괄, 검증 후 덧셈 계산 결과 반환
         Separator separator = new Separator();
         InputValidator validator = new InputValidator();
-        if(validator.isZero(input)){
+        
+        if(validator.isZero(input)){ //문자열 비어있으면 리턴
             return;
         }
-        
         String[] sep_list = separator.separate(input); //분리
         validator.isRight(sep_list); //검증
-        System.out.println(Arrays.toString(sep_list)); //분리 잘됐는지 확인용 배열 출력
 
         for(String s : sep_list){ //덧셈 진행
             if (s.isEmpty()) continue;
             sum += Integer.parseInt(s.trim());
         }
-
     }
     
     public void run(){ //Application에 들어갈 메소드
@@ -35,10 +33,7 @@ public class Calculator {
 
         output.printStart();
         String newInput = input.newInput();
-
         Calculate(newInput);
-        output.printResult(sum);  // 성공한 경우에만 출력
-
-
+        output.printResult(sum);
     }
 }
--- src/main/java/calculator/domain/InputValidator.java
+++ src/main/java/calculator/domain/InputValidator.java
@@ -7,12 +7,13 @@ public class InputValidator { //검증
             throw new IllegalArgumentException(" 커스텀 구분자 설정이 잘못되었습니다.");
         }
     }
+
     public void isRight(String[] list) { //입력한 문자열이 정상적인지 확인
         only_num_check(list);
         list_check(list);
     }
 
-    private static void list_check(String[] list) { //연속된 구분자 있는지 확인
+    private void list_check(String[] list) { //연속된 구분자 있는지 확인
         for(int i = 0; i < list.length; i++){
             if((i >0 && i < list.length-1) && list[i].isEmpty()){
                 throw new IllegalArgumentException("구분자가 연속되어 사용되었습니다.");
@@ -22,16 +23,20 @@ public class InputValidator { //검증
         }
     }
 
-    private static void only_num_check(String[] list) { //숫자만 입력되었는지 검증
+    private void only_num_check(String[] list) { //숫자만 입력되었는지 검증
         if (list.length == 1 && Integer.parseInt(list[0]) >0){
             throw new IllegalArgumentException("숫자만 입력되었습니다.");
         }
     }
 
-    private static void sep_check(String s) { //분리된 문자열이 원하는 형태로 잘 변환될 수 있는지 확인
+    private void sep_check(String s) { //분리된 문자열이 원하는 형태로 잘 변환될 수 있는지 확인
         if (s.isEmpty()){
             return;
         }
+        string_parse_int(s);
+    }
+
+    private void string_parse_int(String s) {
         try {
             Integer.parseInt(s);
             if (Integer.parseInt(s) < 0) {
